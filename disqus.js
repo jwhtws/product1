0ea472b00690ea0676a5b2138a8b741a6d287afb
jwhtws <jwhtws3262@gmail.com>
2026-07-27 13:00:55 +0000

    refactor: 문자 인코딩 정리와 기능별 코드 분리

---
+++ disqus.js
@@ -0,0 +1,1 @@
+var disqus_config=function(){this.page.url=window.location.href;this.page.identifier='meokdang-home'};(function(){var d=document,s=d.createElement('script');s.src='https://product1-5.disqus.com/embed.js';s.setAttribute('data-timestamp',+new Date());(d.head||d.body).appendChild(s)})();
